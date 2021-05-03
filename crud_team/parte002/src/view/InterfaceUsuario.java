d550bd380e7111c386b0c78b186f32cc5220c39a
Diogo-Miranda <damiranda@sga.pucminas.br>
2021-05-02 21:51:39 -0300

    update interface

--- crud_team/parte002/src/view/InterfaceUsuario.java
+++ crud_team/parte002/src/view/InterfaceUsuario.java
@@ -64,9 +64,10 @@ public class InterfaceUsuario {
 		}
 	}
 
-	public void Menu() throws Exception {
+	public boolean Menu() throws Exception {
 
 		ler = new Scanner(System.in);
+		boolean isLogin = false;
 		do {
 			if (forcarEntrada) {
 				entrada = entradaForcada;
@@ -82,11 +83,10 @@ public class InterfaceUsuario {
 
 			switch (entrada) {
 			case "1":
-				Login();
+				isLogin = Login();
 				break;
-
 			case "2":
-				Cadastro();
+				isLogin = Cadastro();
 				break;
 
 			case "0":
@@ -100,6 +100,7 @@ public class InterfaceUsuario {
 
 		} while (!entrada.equals("0"));
 
+		return isLogin;
 	}
 
 	public void ImprimirMenuPrincipalPerguntas() {
@@ -126,7 +127,7 @@ public class InterfaceUsuario {
 		new File(DATA_FOLDER + "usuarios.hash_d.db").delete();
 	}
 
-	public void Login() throws Exception {
+	public boolean Login() throws Exception {
 		System.out.println("------------------------------");
 		System.out.println("LOGIN");
 		System.out.println("------------------------------");
@@ -136,6 +137,8 @@ public class InterfaceUsuario {
 		System.out.print("\t-> ");
 		email = ler.nextLine();
 
+		boolean isLogin = false;
+
 		// Buscar o ID do usuário usando o índice secundário externo (de e-mails);
 		int id = secondaryIndex.read(email);
 		if (id != -1) {
@@ -144,73 +147,71 @@ public class InterfaceUsuario {
 			System.out.println("| Insira sua senha: ");
 			System.out.print("\t-> ");
 			senha = ler.nextLine();
-            
+		
 			if (user.getSenha() == senha.hashCode()) {
 				// Senha correta
 				System.out.println("| Acesso garantido -> tela principal.");
-
+				isLogin = true;
 			} else {
 				System.out.println("| ERRO: senha incorreta.");
 			}
-			return;
 		} else {
 			System.out.println("| ERRO: Email não cadastrado.");
-			return;
 		}
-
+		
+		return isLogin;
 	}
 
-	public void Cadastro() throws Exception {
-
+	public boolean Cadastro() throws Exception {
 		print_user_info();
 
 		System.out.println("| Insira seu email:");
 		System.out.print("\t-> ");
 		email = ler.nextLine();
 		email = email.toLowerCase();
+		boolean isLogin = false;
 
-		if (email.isEmpty())
-			return;
+		if (!email.isEmpty()) {
 
-		int idEncontrado = secondaryIndex.read(email);
+			int idEncontrado = secondaryIndex.read(email);
 
-		if (idEncontrado > 0) {
-			System.out.println("| Email ja cadastrado.");
-			entradaForcada = "1";
-			forcarEntrada = true;
-			return;
+			if (idEncontrado > 0) {
+				System.out.println("| Email ja cadastrado.");
+				entradaForcada = "1";
+				forcarEntrada = true;
+			} else {
+				System.out.println("| Insira seu nome: ");
+				System.out.print("\t-> ");
+				nome = ler.nextLine();
+
+				String confirmarSenha;
+
+				do {
+					System.out.println("| Insira sua senha: ");
+					System.out.print("\t-> ");
+					senha = ler.nextLine();
+
+					System.out.println("| Confirme sua senha: ");
+					System.out.print("\t-> ");
+					confirmarSenha = ler.nextLine();
+
+				} while (!confirmarSenha.equals(senha));
+
+				System.out.println("| Confirmar cadastro? (S/N)");
+				System.out.print("\t-> ");
+				confirmar = ler.nextLine();
+
+				if (confirmar.toUpperCase().equals("S")) {
+					// informar usuario criado;
+					int id = arqUsuario.create(new Usuario(nome, email, senha));
+					secondaryIndex.create(email, id);
+					System.out.println("| Usuario Criado com sucesso.");
+					isLogin = true;
+				}
+			}
 		}
 
-		System.out.println("| Insira seu nome: ");
-		System.out.print("\t-> ");
-		nome = ler.nextLine();
-
-		String confirmarSenha;
-
-		do {
-			System.out.println("| Insira sua senha: ");
-			System.out.print("\t-> ");
-			senha = ler.nextLine();
-
-			System.out.println("| Confirme sua senha: ");
-			System.out.print("\t-> ");
-			confirmarSenha = ler.nextLine();
-
-		} while (!confirmarSenha.equals(senha));
-
-		System.out.println("| Confirmar cadastro? (S/N)");
-		System.out.print("\t-> ");
-		confirmar = ler.nextLine();
-
-		if (confirmar.toUpperCase().equals("S")) {
-			// informar usuario criado;
-			int id = arqUsuario.create(new Usuario(nome, email, senha));
-			secondaryIndex.create(email, id);
-			System.out.println("| Usuario Criado com sucesso.");
-		}
-
-		return;
-
+		return isLogin;
 	}
 
 	void print_user_info() {
